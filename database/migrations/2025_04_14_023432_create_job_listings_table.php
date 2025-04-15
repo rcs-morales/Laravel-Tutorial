f5592bf48b09f74e4c44472dd33d10667b5b590d
Raymund Morales <181067701+rcs-morales@users.noreply.github.com>
2025-04-15 09:52:53 +0800

    Add tables and models for employer and tag

--- database/migrations/2025_04_14_023432_create_job_listings_table.php
+++ database/migrations/2025_04_14_023432_create_job_listings_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Employer;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
             // $table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(\App\Models\Employer::class);
+            $table->foreignIdFor(Employer::class);
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
