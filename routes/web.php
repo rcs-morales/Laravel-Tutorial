2cfae75443fbaccefb9826424a20b9393afc2c78
Raymund Morales <181067701+rcs-morales@users.noreply.github.com>
2025-04-21 10:11:42 +0800

    refactor: add resource method

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController; 
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home' ); 
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-Route::get('/contact', function () {
-    
-    return view('contact');
-
-});
+Route::resource('jobs', JobController::class);
