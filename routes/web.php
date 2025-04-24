47098e2a29fd145ec88ba309861d9dcba292a23a
Raymund Morales <181067701+rcs-morales@users.noreply.github.com>
2025-04-24 09:02:14 +0800

    feat: add send and preview using mailable classesc

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 
+
+
 Route::view('/', 'home' ); 
 Route::view('/contact', 'contact');
 
